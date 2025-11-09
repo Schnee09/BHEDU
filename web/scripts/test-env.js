bc22cc1aa5332546719e8c8be8eec232c0d60ac7
Schnee09 <infinityknight2001@gmail.com>
2025-11-09 17:44:11 +0700

    fix: convert test scripts to ES modules for ESLint compliance

--- web/scripts/test-env.js
+++ web/scripts/test-env.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+import crypto from 'crypto';
 
 const key = process.env.INTERNAL_API_KEY;
 console.log('Key from env:', JSON.stringify(key));
--- web/scripts/verify-sig.js
+++ web/scripts/verify-sig.js
@@ -1,4 +1,5 @@
-const crypto = require('crypto');
+import crypto from 'crypto';
+
 const key = '23685fb84b01f55cf50d871a6d71e5bc3d12e01a48b95e54986ab279521fa5b6';
 const empty = '';
 const hmac = crypto.createHmac('sha256', key).update(empty).digest('hex');
